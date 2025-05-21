e2b2522f8f2b06b87f8717bed125458da6d89b48
angelesortega <mariaortega5l@outlook.com>
2025-05-22 00:15:09 +0200

    guardando clases de producto y persona

---
+++ src/main/java/com/maol/demo/taller/atributos/dto/Movil.java
@@ -0,0 +1,9 @@
+package com.maol.demo.taller.atributos.dto;
+
+public class Movil {
+
+	private String marca = "samsung";
+	private int añosDeGarantia = 4;
+	private double CantidadDeTeclas = 355.5;
+			
+}
---
+++ src/main/java/com/maol/demo/taller/atributos/dto/Persona.java
@@ -0,0 +1,11 @@
+package com.maol.demo.taller.atributos.dto;
+
+public class Persona {
+
+	private String nombre = "afbh";
+	private int maratonesHechas = 2;
+	private double distanciaRecorrida = 21.5;
+	
+	
+	
+}
