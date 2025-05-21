be8551020cec1ce238547575426427401b721d9f
angelesortega <mariaortega5l@outlook.com>
2025-05-21 23:55:13 +0200

    guardando clases coche y leon

---
+++ src/main/java/com/maol/demo/taller/atributos/dto/Coche.java
@@ -0,0 +1,11 @@
+package com.maol.demo.taller.atributos.dto;
+
+public class Coche {
+
+	private String marca = "peugeot";
+	private int modelo = 2003;
+	private double NumeroDePuertas = 6;
+	
+	
+	
+}
---
+++ src/main/java/com/maol/demo/taller/atributos/dto/Leon.java
@@ -0,0 +1,11 @@
+package com.maol.demo.taller.atributos.dto;
+
+public class Leon {
+
+	
+	private String raza = "mestizo";
+	private int numeroDePatas = 4;
+	private double peso = 98.7;
+	
+	
+}
