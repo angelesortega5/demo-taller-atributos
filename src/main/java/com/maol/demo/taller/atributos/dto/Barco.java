12870dfd9053ba472c1942a3ff8234a1056af826
angelesortega <mariaortega5l@outlook.com>
2025-05-22 00:05:04 +0200

    guardando clases de barco y bicicleta

---
+++ src/main/java/com/maol/demo/taller/atributos/dto/Barco.java
@@ -0,0 +1,11 @@
+package com.maol.demo.taller.atributos.dto;
+
+public class Barco {
+
+	private String nombre = "laviguesa";
+	private int AñosDeUso = 150;
+	private double numeroDeTripulantes = 255.2;
+	
+	
+	
+}
---
+++ src/main/java/com/maol/demo/taller/atributos/dto/Bicicleta.java
@@ -0,0 +1,11 @@
+package com.maol.demo.taller.atributos.dto;
+
+public class Bicicleta {
+
+	private String nombre = "shakira";
+	private int numeroDeLlantas = 4;
+	private double Velocidades = 6.8;
+	
+	
+	
+}
